1f03fa949253a997a75c32d44d9ebaaa56673d9b
RimaAvetisyan <67859597+RimaAvetisyan@users.noreply.github.com>
2021-10-31 17:34:44 +0400

    Add files via upload

---
+++ ternar operator(3 & 7 bazmapatik tver).cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mutqagreq tiv");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n % 21 == 0 ? "bajanarar e" : "bajanarar che");
+            Console.ReadKey();
+        }
+    }
+}
