6119d2af180f12add6abe3797945f32d11ab8a32
RimaAvetisyan <67859597+RimaAvetisyan@users.noreply.github.com>
2021-11-05 09:50:08 +0400

    Add files via upload

---
+++ gtnel faktoryaly & tveri qanaky.cs
@@ -0,0 +1,3 @@
+﻿1>------ Build started: Project: ConsoleApp38, Configuration: Debug Any CPU ------
+1>  ConsoleApp38 -> C:\Users\Help\source\repos\ConsoleApp38\ConsoleApp38\bin\Debug\ConsoleApp38.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
